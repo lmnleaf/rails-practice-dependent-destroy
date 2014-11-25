ac0c981c37fa2358d1461eb212bffab2b54a2d13
lmnleaf <larkfleming@gmail.com>
2014-11-24 19:35:57 -0700

    Adds prescription count to homepage.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,7 @@ class DashboardController < ApplicationController
   def index
     @patients = Patient.order(:first_name, :last_name)
     @medications = Medication.order(:name)
+    @prescriptions = Prescription.all
   end
 
 end
